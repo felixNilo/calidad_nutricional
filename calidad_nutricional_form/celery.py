4ce7a4cb41de49621e5d3a0c203141c1e99fbab3
Felix Nilo <fnilo@buk.cl>
2025-01-30 12:16:54 -0300

    celery cron

--- calidad_nutricional_form/celery.py
+++ calidad_nutricional_form/celery.py
@@ -2,6 +2,7 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 # Establecer el módulo de configuración de Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "calidad_nutricional_form.settings")
@@ -13,3 +14,10 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Autodiscover tasks.py en las aplicaciones instaladas
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    "notify-admin-unmatched-searches": {
+        "task": "calidad_nutricional_form.tasks.notify_admin_unmatched_searches",
+        "schedule": crontab(hour=23, minute=59),  # Ejecutar al final del día
+    },
+}
