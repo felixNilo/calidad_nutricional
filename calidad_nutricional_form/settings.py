3708d77db7a08d1ff24d3df5cf1223679bd3fffd
Felix Nilo <fnilo@buk.cl>
2025-02-11 20:03:03 -0300

    removed middleware

--- calidad_nutricional_form/settings.py
+++ calidad_nutricional_form/settings.py
@@ -72,8 +72,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'initial_form.middleware.FlowValidationMiddleware',
-
 ]
 
 ROOT_URLCONF = 'calidad_nutricional_form.urls'
